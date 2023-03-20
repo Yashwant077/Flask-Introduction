fdfa7061c27076b5f1aa96547decea05a2dd1ccb
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2023-03-20 21:30:04 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+"""
+If we hit enter run on this it will show successful execution but will not print anything.
+But, here we are creating our website and that we can access through a server.
+So, we will need to understand here that:
+
+How to create local server on windows when working on flask?
+Step-1 Create your .py file and write code as we have hello.py here.
+Step-2 Check if your code have any errors by simply running hello.py file.
+Step-3 Apply command -> set FLASK_APP=hello.py
+Step-4 Apply command -> $env:FLASK_APP="hello.py"
+Step-5 Apply command -> flask run
+Step-6 Now you will have a server address from flask run output
+Step-7 It is your local server, just click on it to access your website.
+"""
